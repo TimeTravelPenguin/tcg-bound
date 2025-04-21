958ad472f2b68ada6136395ff37df06fe78bf8d9
Phillip Smith <35803538+TimeTravelPenguin@users.noreply.github.com>
2025-04-21 17:40:56 +1000

    initial commit

---
+++ src/app.rs
@@ -0,0 +1,89 @@
+use egui::{Theme, ThemePreference, Ui};
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(PartialEq, serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct ShaderApp {
+    pub max: i32,
+    pub value: i32,
+}
+
+impl Default for ShaderApp {
+    fn default() -> Self {
+        Self { max: 256, value: 1 }
+    }
+}
+
+impl ShaderApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for ShaderApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                // NOTE: no File->Quit on web pages!
+                let is_web = cfg!(target_arch = "wasm32");
+                if !is_web {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_theme_preference_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("TCG Bound");
+
+            ui.horizontal(|ui| {
+                ui.label("Max: ");
+                ui.add(egui::DragValue::new(&mut self.max).range(1..=i32::MAX));
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Card Number: ");
+                ui.add(egui::Slider::new(&mut self.value, 1..=self.max));
+            });
+
+            ui.separator();
+
+            ui.add(egui::github_link_file!(
+                "https://github.com/TimeTravelPenguin/tcg-bound/blob/main/",
+                "Source code."
+            ));
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                egui::warn_if_debug_build(ui);
+            });
+        });
+    }
+}
