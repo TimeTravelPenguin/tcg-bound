3b1105b2409d128d7399d95e79085b84585d1949
Phillip Smith <35803538+TimeTravelPenguin@users.noreply.github.com>
2025-05-06 00:33:40 +1000

    refactor!: reworked to use dedicated types to avoid ambiguity + fix bugs with visual grid

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,28 @@
-use egui::{Align, Button, Label, Layout, Separator, Slider, TextEdit, Ui, Vec2, Widget};
+use std::num::NonZeroU32;
+
+use crate::{
+    binder::{Binder, BinderSlot},
+    card_number::{CardNumber, SlotIndex},
+};
+use egui::{Button, Label, Slider, Ui, Vec2, Widget};
 use egui_extras::{Column, TableBuilder};
 use egui_flex::{item, Flex, FlexAlignContent};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(PartialEq, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, PartialEq, serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct App {
-    pub value: u32,
-    pub max_value: u32,
-    pub rows: u16,
-    pub cols: u16,
-    pub pages: u16,
+    pub value: CardNumber,
+    pub max_value: NonZeroU32,
+    pub binder: Binder,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            value: 1,
-            max_value: 300,
-            rows: 3,
-            cols: 3,
-            pages: 20,
+            value: CardNumber::try_new(1, 100).expect("Default value should be 1"),
+            max_value: NonZeroU32::new(100).expect("Default value should be 100"),
+            binder: Binder::new(3, 3, 20),
         }
     }
 }
@@ -39,29 +41,9 @@ impl App {
 
         Default::default()
     }
-
-    pub fn page_slots(&self) -> u32 {
-        (self.rows * self.cols) as u32
-    }
-
-    pub fn current_page(&self) -> u32 {
-        self.value.div_euclid(self.page_slots()) + 1
-    }
-
-    pub fn current_slot(&self) -> u32 {
-        self.value % self.page_slots() + 1 // Shift to 1-based index
-    }
-
-    pub fn current_slot_row(&self) -> u32 {
-        (self.value % self.page_slots()) / self.cols as u32
-    }
-
-    pub fn current_slot_col(&self) -> u32 {
-        (self.value % self.page_slots()) % self.cols as u32
-    }
 }
 
-fn draw_table(ui: &mut Ui, app: &mut App) {
+fn settings_table(ui: &mut Ui, app: &mut App) -> anyhow::Result<()> {
     TableBuilder::new(ui)
         .column(Column::auto().resizable(false))
         .column(Column::remainder())
@@ -82,8 +64,18 @@ fn draw_table(ui: &mut Ui, app: &mut App) {
                 row.col(|ui| {
                     ui.label("Rows");
                 });
+
+                let mut rows = app.binder.rows();
                 row.col(|ui| {
-                    ui.add(egui::DragValue::new(&mut app.rows).range(1..=u16::MAX));
+                    if ui
+                        .add(egui::DragValue::new(&mut rows).range(1..=u16::MAX))
+                        .changed()
+                    {
+                        let prev_state = app.binder;
+                        if app.binder.set_rows(rows).is_err() {
+                            app.binder = prev_state;
+                        }
+                    }
                 });
             });
 
@@ -91,8 +83,18 @@ fn draw_table(ui: &mut Ui, app: &mut App) {
                 row.col(|ui| {
                     ui.label("Columns");
                 });
+
+                let mut cols = app.binder.cols();
                 row.col(|ui| {
-                    ui.add(egui::DragValue::new(&mut app.cols).range(1..=u16::MAX));
+                    if ui
+                        .add(egui::DragValue::new(&mut cols).range(1..=u16::MAX))
+                        .changed()
+                    {
+                        let prev_state = app.binder;
+                        if app.binder.set_cols(cols).is_err() {
+                            app.binder = prev_state;
+                        }
+                    }
                 });
             });
 
@@ -100,8 +102,18 @@ fn draw_table(ui: &mut Ui, app: &mut App) {
                 row.col(|ui| {
                     ui.label("Pages");
                 });
+
+                let mut pages = app.binder.pages();
                 row.col(|ui| {
-                    ui.add(egui::DragValue::new(&mut app.pages).range(1..=u16::MAX));
+                    if ui
+                        .add(egui::DragValue::new(&mut pages).range(1..=u16::MAX))
+                        .changed()
+                    {
+                        let prev_state = app.binder;
+                        if app.binder.set_pages(pages).is_err() {
+                            app.binder = prev_state;
+                        }
+                    }
                 });
             });
 
@@ -110,29 +122,61 @@ fn draw_table(ui: &mut Ui, app: &mut App) {
                     ui.label("Max Value");
                 });
                 row.col(|ui| {
-                    ui.add(egui::DragValue::new(&mut app.max_value).range(1..=u32::MAX));
+                    ui.add(egui::DragValue::new(&mut app.max_value).range(1..=u16::MAX));
                 });
             });
         });
+
+    Ok(())
 }
 
-fn card_table(id: &str, ui: &mut Ui, app: &mut App, is_left_page: bool) {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum CardTable {
+    Left,
+    Right,
+}
+
+fn card_table(id: &str, ui: &mut Ui, app: &mut App, page_side: CardTable) {
     let card_ratio = Vec2::new(2.5, 3.5);
+
+    let binder_rows = app.binder.rows();
+    let binder_cols = app.binder.cols();
 
     TableBuilder::new(ui)
         .id_salt(id)
         .vscroll(false)
-        .columns(Column::auto().resizable(false), app.cols as usize)
-        .body(|mut body| {
-            body.rows(20.0, app.rows.into(), |mut row| {
-                let is_current_page = app.current_page() % 2 == is_left_page as u32;
+        .columns(Column::auto().resizable(false), binder_cols as usize)
+        .body(|body| {
+            body.rows(20.0, binder_rows as usize, |mut row| {
+                for col in 0..binder_cols {
+                    let page_offset = app.value.to_index().get() / (binder_rows * binder_cols);
+                    let current_cell_index = SlotIndex::new(
+                        (row.index() as u32 * binder_cols + col)
+                            + page_offset * binder_rows * binder_cols,
+                    );
 
-                for col in 0..app.cols {
-                    let selected = is_current_page
-                        && app.current_slot_row() == row.index() as u32
-                        && app.current_slot_col() == col as u32;
+                    // Check if the current slot is the selected one
+                    // The selected slot is the one that matches the current card number
+                    // and is on the correct page
+                    let current_slot = BinderSlot::from_card_number(&app.binder, app.value);
 
-                    let dbg_label = format!("Row: {}, Col: {}", row.index(), col);
+                    // Check if the current user-selected card slot is on the correct page
+                    let correct_page = match page_side {
+                        CardTable::Left => page_offset % 2 == 0,
+                        CardTable::Right => page_offset % 2 == 1,
+                    };
+
+                    let current_slot_selected =
+                        correct_page && current_slot.index() == current_cell_index;
+
+                    let dbg_label = format!(
+                        "Row: {}, Col: {}\nPage: {}\nCorrect page: {}\nSelected: {}",
+                        row.index(),
+                        col,
+                        current_slot.page(),
+                        correct_page,
+                        current_slot_selected
+                    );
 
                     let dbg_label = cfg!(debug_assertions)
                         .then(|| dbg_label)
@@ -141,7 +185,7 @@ fn card_table(id: &str, ui: &mut Ui, app: &mut App, is_left_page: bool) {
                     row.col(|ui| {
                         Button::new(dbg_label)
                             .min_size(card_ratio * 20.0)
-                            .selected(selected)
+                            .selected(current_slot_selected)
                             .ui(ui);
                     });
                 }
@@ -150,7 +194,6 @@ fn card_table(id: &str, ui: &mut Ui, app: &mut App, is_left_page: bool) {
 }
 
 impl eframe::App for App {
-    /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
@@ -194,13 +237,26 @@ impl eframe::App for App {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("TCG Bound");
 
-            draw_table(ui, self);
+            settings_table(ui, self).expect("Failed to create settings table");
 
             ui.separator();
 
             Flex::vertical().show(ui, |flex| {
                 flex.add(item().shrink(), Label::new("Card Number:"));
-                flex.add(item(), Slider::new(&mut self.value, 1..=self.max_value));
+
+                let mut slider_value = self.value.get();
+                if flex
+                    .add(
+                        item(),
+                        Slider::new(&mut slider_value, 1..=self.max_value.get()),
+                    )
+                    .changed()
+                {
+                    if let Some(new_value) = CardNumber::try_new(slider_value, self.max_value.get())
+                    {
+                        self.value = new_value;
+                    }
+                }
 
                 flex.add_flex(
                     item().grow(1.0),
@@ -212,43 +268,58 @@ impl eframe::App for App {
                             .add(item().min_height(20.0), Button::new("-1"))
                             .clicked()
                         {
-                            self.value = self.value.saturating_sub(1).max(1);
+                            self.value = CardNumber::try_new(
+                                self.value.get().saturating_sub(1),
+                                self.max_value.get(),
+                            )
+                            .unwrap_or(self.value);
                         }
 
                         if flex
                             .add(item().min_height(20.0), Button::new("+1"))
                             .clicked()
                         {
-                            self.value = self.value.saturating_add(1).min(self.max_value);
+                            self.value = CardNumber::try_new(
+                                self.value.get().saturating_add(1),
+                                self.max_value.get(),
+                            )
+                            .unwrap_or(self.value);
                         }
                     },
                 );
 
                 if flex.add(item().grow(1.0), Button::new("Reset")).clicked() {
-                    self.value = 1;
+                    self.value = CardNumber::try_new(1, self.max_value.get())
+                        .expect("Default value should be 1");
                 }
             });
 
             ui.separator();
+
+            let slot = BinderSlot::from_index(&self.binder, self.value.to_index());
 
             ui.vertical(|ui| {
                 ui.label("Card binder location: ");
 
                 ui.horizontal(|ui| {
                     ui.label("Page: ");
-                    ui.label(format!("{}/{}", self.current_page(), self.pages));
+                    ui.label(format!("{}/{}", slot.page(), self.binder.pages()));
                 });
 
                 ui.horizontal(|ui| {
-                    ui.label("Slot: ");
-                    ui.label(format!("{}/{}", self.current_slot(), self.page_slots()));
+                    ui.label("Slot: "); // Slot on the current page
+                    ui.label(format!(
+                        "{}/{}",
+                        slot.index().get() % self.binder.total_page_slots() + 1,
+                        self.binder.total_page_slots()
+                    ));
                 });
             });
 
             ui.horizontal(|ui| {
-                card_table("visual_table_left", ui, self, true);
+                card_table("visual_table_left", ui, self, CardTable::Left);
                 ui.separator();
-                card_table("visual_table_right", ui, self, false);
+                card_table("visual_table_right", ui, self, CardTable::Right);
             });
         });
     }
